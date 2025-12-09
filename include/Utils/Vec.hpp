f7ed5d6846c46ebe12bfe7d2c61eea762df60ee2
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2025-12-09 21:20:36 +0100

    Add Vec

---
+++ include/Utils/Vec.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class Vec
+{
+private:
+public:
+    Vec();
+    Vec(float x, float y);
+    ~Vec();
+    float x, y;
+};
+
---
+++ src/Utils/Vec.cpp
@@ -0,0 +1,16 @@
+#include <Utils/Vec.hpp>
+
+
+Vec::Vec():x(0), y(0)
+{
+
+}
+
+Vec::Vec(float x, float y): x(x), y(y)
+{
+
+}
+
+Vec::~Vec(){
+
+}
