e0e654a6233cb0d27437361b949e6fd1cdd8244a
Tynours <pitynours@gmail.com>
2025-12-10 21:25:28 +0100

    vec done

--- include/Utils/Vec.hpp
+++ include/Utils/Vec.hpp
@@ -1,12 +1,28 @@
 #pragma once
+#include <cmath>
 
 class Vec
 {
 private:
 public:
     Vec();
-    Vec(float x, float y);
+    Vec(double x, double y);
     ~Vec();
-    float x, y;
+    double x, y;
+
+    //Operators
+    Vec& operator+=(const Vec& other);
+    Vec& operator-=(const Vec& other);
+    Vec& operator*=(double scalar);
+    Vec& operator/=(double scalar);
+    Vec operator-();
 };
 
+Vec operator+(Vec a, const Vec& b);
+Vec operator-(Vec a, const Vec& b);
+Vec operator*(Vec a, float scalar);
+Vec operator*(float scalar, Vec a);
+Vec operator/(Vec a, float scalar);
+double dot(const Vec& a,const Vec& b);
+double dist_sq(const Vec& a,const Vec& b);
+double dist(const Vec& a,const Vec& b);
--- src/Utils/Vec.cpp
+++ src/Utils/Vec.cpp
@@ -6,11 +6,76 @@ Vec::Vec():x(0), y(0)
 
 }
 
-Vec::Vec(float x, float y): x(x), y(y)
+Vec::Vec(double x, double y): x(x), y(y)
 {
 
 }
 
 Vec::~Vec(){
 
+}
+
+Vec& Vec::operator+=(const Vec& other){
+    x+=other.x;
+    y+=other.y;
+    return *this;
+}
+
+Vec& Vec::operator-=(const Vec& other){
+    x-=other.x;
+    y-=other.y;
+    return *this;
+}
+
+Vec Vec::operator-(){
+    return Vec(-x,-y);
+}
+
+Vec& Vec::operator*=(double scalar){
+    x*=scalar;
+    y*=scalar;
+    return *this;
+}
+
+Vec& Vec::operator/=(double scalar){
+    x/=scalar;
+    y/=scalar;
+    return *this;
+}
+
+Vec operator+(Vec a, const Vec& b){
+    a+=b;
+    return a;
+}
+
+Vec operator-(Vec a, const Vec& b){
+    a-=b;
+    return a;
+}
+
+Vec operator*(float scalar, Vec a){
+    a*=scalar;
+    return a;
+}
+
+Vec operator*(Vec a, float scalar){
+    a*=scalar;
+    return a;
+}
+
+Vec operator/(Vec a, float scalar){
+    a/=scalar;
+    return a;
+}
+
+double dot(const Vec& a,const Vec& b){
+    return a.x*b.x+a.y*b.y;
+}
+
+double dist_sq(const Vec& a,const Vec& b){
+    return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
+}
+
+double dist(const Vec& a,const Vec& b){
+    return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
 }
