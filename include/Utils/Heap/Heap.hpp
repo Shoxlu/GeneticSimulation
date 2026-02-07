dc22e0d0257a71f505c1eae4c994f27f70d418d9
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2026-02-07 19:38:08 +0100

    HeapMax + SortDescending

--- include/Utils/Heap/Heap.hpp
+++ include/Utils/Heap/Heap.hpp
@@ -20,6 +20,25 @@ protected:
     {
         return (i - 1) / 2;
     }
+    size_t PercolateUp(size_t i){
+        size_t father = Father(i);
+        while(i > 0 && data[i] < data[father]){
+            T temp = data[father];
+            data[father] = data[i];
+            data[i] = temp;
+            i = father;
+            father = Father(i);
+        }
+        return i;
+    }
+public:
+    HeapMin()
+    {
+    }
+    HeapMin(size_t size)
+    {
+        data.reserve(size);
+    }
     size_t PercolateDown(size_t i){
         size_t r_child = Right(i);
         size_t l_child = Left(i);
@@ -39,25 +58,6 @@ protected:
             PercolateDown(min);
         }
         return i;
-    }
-    size_t PercolateUp(size_t i){
-        size_t father = Father(i);
-        while(i > 0 && data[i] < data[father]){
-            T temp = data[father];
-            data[father] = data[i];
-            data[i] = temp;
-            i = father;
-            father = Father(i);
-        }
-        return i;
-    }
-public:
-    HeapMin()
-    {
-    }
-    HeapMin(size_t size)
-    {
-        data.reserve(size);
     }
     size_t Push(T elm)
     {
@@ -92,6 +92,116 @@ public:
         return data.size();
     }
 };
+
+template<typename T>
+class HeapMax
+{
+protected:
+    std::vector<T> data;
+    size_t Right(size_t i)
+    {
+        return 2 * i + 2;
+    }
+    size_t Left(size_t i)
+    {
+        return 2 * i + 1;
+    }
+    size_t Father(size_t i)
+    {
+        return (i - 1) / 2;
+    }
+    size_t PercolateUp(size_t i){
+        size_t father = Father(i);
+        while(i > 0 && data[i] > data[father]){
+            T temp = data[father];
+            data[father] = data[i];
+            data[i] = temp;
+            i = father;
+            father = Father(i);
+        }
+        return i;
+    }
+public:
+    HeapMax()
+    {
+    }
+    HeapMax(std::vector<T> tab)
+    {
+        data = tab;
+        size_t size_half = data.size()/2;
+        for (int i = size_half-1; i >= 0; i--)
+        {
+            PercolateDown(i);
+        }
+    }
+    HeapMax(size_t size)
+    {
+        data.reserve(size);
+    }
+    size_t PercolateDown(size_t i){
+        size_t r_child = Right(i);
+        size_t l_child = Left(i);
+        size_t max = i;
+        if(l_child < data.size() && data[max] < data[l_child])
+        {
+            max = l_child;
+        }
+        if(r_child < data.size() && data[max] < data[r_child])
+        {
+            max = r_child;
+        }
+        if(max != i){
+            T temp = data[max];
+            data[max] = data[i];
+            data[i] = temp;
+            PercolateDown(max);
+        }
+        return i;
+    }
+    size_t Push(T elm)
+    {
+        data.emplace_back(elm);
+        return PercolateUp(data.size() - 1);
+    }
+    T& Top(){
+        return data[0];
+    }
+    T Pop(){
+        assert(!IsEmpty());
+        T elm = Top();
+        T last = data[data.size()-1];
+        data.pop_back();
+        if(!IsEmpty()){
+            data[0] = last;
+            PercolateDown(0);
+        }
+        return elm;
+    }
+    bool IsEmpty(){
+        return data.size() == 0;
+    }
+    T operator[](size_t i)
+    {
+        return data[i];
+    }
+    std::vector<T> &GetRawData(){
+        return data;
+    }
+    size_t size(){
+        return data.size();
+    }
+};
+template<typename T>
+void HeapSortDescending(std::vector<T>& tab)
+{
+    HeapMax heap(tab);
+    for(size_t i = 0; i < tab.size(); i++)
+    {
+        T obj = heap.Pop();
+        tab[i] = obj;
+        heap.PercolateDown(i);
+    }
+}
 
 template<typename T>
 class OrderedPair
